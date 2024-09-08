7ef4e882bff32bdba5839ca915f4dbb24d645ea2
KrishnaKalra <krishna26kalra@gmail.com>
2024-09-08 14:24:11 +0530

    Adding server

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from 'cors';
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+const port=5173;
+app.listen(port,()=>{
+    console.log("Listenin to port "+port);
+})
