af60b61b43b12c65dbe580f062af071807e9061d
KrishnaKalra <krishna26kalra@gmail.com>
2024-09-29 14:19:47 +0530

    DataTypeChange

--- index.js
+++ index.js
@@ -161,14 +161,71 @@ catch(err){
 const User26=mongoose.model("year2026",UserSchema);
 app.get('/api/2026',async (req,res)=>{
     let datadb= await (User26.find({}).sort({id:1}));
-    console.log(datadb);
-    res.json(datadb);
+    let arr=[]
+    datadb.map((data)=>{
+        let NewObject={
+            name:data.name,
+            id:data.id
+        };
+        if(data.Description){
+            NewObject.Description=data.Description;
+        }
+        if(data.GitHub){
+            NewObject.GitHub=data.GitHub;
+        }
+        if(data.Instagram){
+            NewObject.Instagram=data.Instagram;
+        }
+        if(data.LinkedIn){
+            NewObject.LinkedIn=data.LinkedIn;
+        }
+        if(data.Location){
+            NewObject.Location=data.Location;
+        }
+
+        if(data.image){
+            NewObject.image=data.image.toString('base64');
+            NewObject.contentType=data.contentType;
+        }
+
+        arr.push(NewObject);
+    });
+    return res.json(arr);
 });
 app.get('/api/2026/id',async (req,res)=>{
+    try{
     const user_id=req.query.id;
     let datadb= await (User26.find({id:user_id}));
-    console.log(datadb);
-    res.json(datadb);
+    let NewObject={
+        name:datadb[0].name,
+        id:datadb[0].id
+    };
+    if(datadb[0].Description){
+        NewObject.Description=datadb[0].Description;
+    }
+    if(datadb[0].GitHub){
+        NewObject.GitHub=datadb[0].GitHub;
+    }
+    if(datadb[0].Instagram){
+        NewObject.Instagram=datadb[0].Instagram;
+    }
+    if(datadb[0].LinkedIn){
+        NewObject.LinkedIn=datadb[0].LinkedIn;
+    }
+    if(datadb[0].Location){
+        NewObject.Location=datadb[0].Location;
+    }
+
+    if(datadb[0].image){
+        NewObject.image=datadb[0].image.toString('base64');
+        NewObject.contentType=datadb[0].contentType;
+    }
+
+    res.json([NewObject]);
+    }
+    catch(err){
+    console.error(err.message);
+    }
 });
 
 app.post('/api/2026/add',async (req,res)=>{
@@ -192,28 +249,30 @@ app.post('/api/2026/add',async (req,res)=>{
 });
 app.post('/api/2026/profile',async(req,res)=>{
     try{
-    const fid=req.body.id;
-    const fimage=req.body.image;
-    const flocation=req.body.location;
-    const fdescription=req.body.description;
-    const fGitHub=req.body.github;
-    const fLinkedIn=req.body.linkedin;
-    const fInstagram=req.body.instagram;
-    console.log(req.body);
-    await User26.findOneAndUpdate({id:fid},{
-        image:fimage,
-        Location:flocation,
-        Description:fdescription,
-        GitHub:fGitHub,
-        Instagram:fInstagram,
-        LinkedIn:fLinkedIn,
-    });
+        let NewObject={
+            Location:req.body.location,
+            Description:req.body.description,
+            GitHub:req.body.github,
+            Instagram:req.body.instagram,
+            LinkedIn:req.body.linkedin,
+        }
+        
+        const fid=req.body.id;
+    
+        if(req.file){
+            const fcontentType=req.file.mimetype;
+            const fimage=req.file.buffer;
+        NewObject['image']=fimage;
+        NewObject['contentType']=fcontentType;
+        }
+    console.log(NewObject);
+    await User26.findOneAndUpdate({id:fid},NewObject);
     res.status(200).json("User Updated");
 }
 catch(err){
     console.error(err.message);
 }
-})
+});
 
 
 
@@ -225,14 +284,71 @@ catch(err){
 const User28=mongoose.model("year2028",UserSchema);
 app.get('/api/2028',async (req,res)=>{
     let datadb= await (User28.find({}).sort({id:1}));
-    console.log(datadb);
-    res.json(datadb);
+    let arr=[]
+    datadb.map((data)=>{
+        let NewObject={
+            name:data.name,
+            id:data.id
+        };
+        if(data.Description){
+            NewObject.Description=data.Description;
+        }
+        if(data.GitHub){
+            NewObject.GitHub=data.GitHub;
+        }
+        if(data.Instagram){
+            NewObject.Instagram=data.Instagram;
+        }
+        if(data.LinkedIn){
+            NewObject.LinkedIn=data.LinkedIn;
+        }
+        if(data.Location){
+            NewObject.Location=data.Location;
+        }
+
+        if(data.image){
+            NewObject.image=data.image.toString('base64');
+            NewObject.contentType=data.contentType;
+        }
+
+        arr.push(NewObject);
+    });
+    return res.json(arr);
 });
 app.get('/api/2028/id',async (req,res)=>{
-    const user_id=req.query.id;
-    let datadb= await (User28.find({id:user_id}));
-    console.log(datadb);
-    res.json(datadb);
+    try{
+        const user_id=req.query.id;
+        let datadb= await (User28.find({id:user_id}));
+        let NewObject={
+            name:datadb[0].name,
+            id:datadb[0].id
+        };
+        if(datadb[0].Description){
+            NewObject.Description=datadb[0].Description;
+        }
+        if(datadb[0].GitHub){
+            NewObject.GitHub=datadb[0].GitHub;
+        }
+        if(datadb[0].Instagram){
+            NewObject.Instagram=datadb[0].Instagram;
+        }
+        if(datadb[0].LinkedIn){
+            NewObject.LinkedIn=datadb[0].LinkedIn;
+        }
+        if(datadb[0].Location){
+            NewObject.Location=datadb[0].Location;
+        }
+    
+        if(datadb[0].image){
+            NewObject.image=datadb[0].image.toString('base64');
+            NewObject.contentType=datadb[0].contentType;
+        }
+    
+        res.json([NewObject]);
+        }
+        catch(err){
+        console.error(err.message);
+        }
 });
 
 app.post('/api/2028/add',async (req,res)=>{
@@ -256,22 +372,24 @@ app.post('/api/2028/add',async (req,res)=>{
 });
 app.post('/api/2028/profile',async(req,res)=>{
     try{
-    const fid=req.body.id;
-    const fimage=req.body.image;
-    const flocation=req.body.location;
-    const fdescription=req.body.description;
-    const fGitHub=req.body.github;
-    const fLinkedIn=req.body.linkedin;
-    const fInstagram=req.body.instagram;
-    console.log(req.body);
-    await User28.findOneAndUpdate({id:fid},{
-        image:fimage,
-        Location:flocation,
-        Description:fdescription,
-        GitHub:fGitHub,
-        Instagram:fInstagram,
-        LinkedIn:fLinkedIn,
-    });
+        let NewObject={
+            Location:req.body.location,
+            Description:req.body.description,
+            GitHub:req.body.github,
+            Instagram:req.body.instagram,
+            LinkedIn:req.body.linkedin,
+        }
+        
+        const fid=req.body.id;
+    
+        if(req.file){
+            const fcontentType=req.file.mimetype;
+            const fimage=req.file.buffer;
+        NewObject['image']=fimage;
+        NewObject['contentType']=fcontentType;
+        }
+    console.log(NewObject);
+    await User28.findOneAndUpdate({id:fid},NewObject);
     res.status(200).json("User Updated");
 }
 catch(err){
