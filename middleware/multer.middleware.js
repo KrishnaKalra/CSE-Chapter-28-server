70265e6c1339eafb55da82816711fa2690f093c7
KrishnaKalra <krishna26kalra@gmail.com>
2024-09-29 19:55:12 +0530

    image type change

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const storage=multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,path.join(__dirname, 'public/temp'))
+        cb(null,path.join(__dirname, '../public/temp'))
     },
     filename: function(req,file,cb){
         cb(null,file.originalname)
